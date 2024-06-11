fb4f0ee434c18063db79cc264079ff8d6224f5c7
lucas_abmael <lucasabmaelbandeira@gmail.com>
2024-06-11 20:17:12 -0300

    Primeiro commit do projeto :100:

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+import Inicio from './components/Inicio';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Inicio />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css";
+
+function Header() {
+    return (
+        <header className={styles.header}>
+            <img src="\imagens\kairos.png"/>
+        </header>
+    );
+};
+
+export default Header;
