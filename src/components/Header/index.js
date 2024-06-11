01fb2c6acef68fd42435cdd268939829cfd5531e
lucas_abmael <lucasabmaelbandeira@gmail.com>
2024-06-11 20:51:54 -0300

    link adicionado

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,9 @@ import styles from "./Header.module.css";
 function Header() {
     return (
         <header className={styles.header}>
-            <img src="\imagens\kairos.png"/>
+            <a href="https://kairos-academia.netlify.app/">
+                <img src="\imagens\kairos.png"/>
+            </a>
         </header>
     );
 };
